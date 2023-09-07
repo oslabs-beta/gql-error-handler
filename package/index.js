e4dbfb7ff82d6cf661eecc05d1645c4a818481e3
Samuel Ryder <samuelryder@Samuels-MacBook-Pro.local>
2023-09-07 18:53:03 -0400

    package/index.js pseudocode

--- package/index.js
+++ package/index.js
@@ -2,4 +2,17 @@
 
 const errorHandlers = {};
 
+// client will employ a single function specified by the package (higher order function) and within the package all of the lower level functionality will be executed through callbacks, abstracting away all of the functionality and complexity from the developer
+
+// intercept original query
+// cache original query
+// return query and send it to query parser
+// get error response from query parser on fail
+// identify type of error (classification)
+// perform specific string parsing (based on classification of error)
+// (various methods of string parsing needed here based on error classification)
+// based on this parse of error message, reformat the original querry in the cache
+// send reformulated querry back to the querry parser for validation (expecting that it will execute)
+// intercept the data response from the DB and then add the original error message (or some portion of it) to the data object to be finally sent to the client
+
 module.exports = errorHandlers;
