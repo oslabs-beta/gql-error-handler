16ceb965b225029f6c2b6ad62ebff904d16c2910
Samuel Ryder <samuelryder@Samuels-MacBook-Pro.local>
2023-09-09 16:39:47 -0400

    errorParser func & fieldDoesntExist func

--- package/errLibrary.ts
+++ package/errLibrary.ts
@@ -2,3 +2,56 @@
 // object on file as library for all error classifications (key will be regular expression of different error message strings, value will be the string manipulation method needed to parse the error message string)
 // take error message from error, compare within classification object
 // parse error message with string manip. method to extract faulty field labels
+
+
+// process string value of message property with regrular expression to return value matching property in library object
+// matching property in library: "Cannot query field on type."
+
+// we have the error message string value and 
+
+// function fieldDoesntExist(error) {
+
+//   const strArr = error.split(' ');
+
+//   const fields = [];
+//   for (let i = 0; i < strArr.length; i++) {
+//     if (strArr[i][0] === '"') {
+//       const regex = /[A-Za-z]+/;
+//       const procEl = strArr[i].match(regex);
+//       fields.push(procEl[0]);
+//     }
+//   }
+
+//   return fields;
+// }
+
+const errLibrary = {
+
+  "Cannot query field on type": function fieldDoesntExist(error) {
+
+    const strArr = error.split(' ');
+
+    const fields = [];
+
+    for (let i = 0; i < strArr.length; i++) {
+      if (strArr[i][0] === '"') {
+        const regex = /[A-Za-z]+/;
+        const procEl = strArr[i].match(regex);
+        fields.push(procEl[0]);
+      }
+    }
+    // return a 2-item array
+    // first item is the name of the field that doesn't exist
+    // second item is the name of the type in the schema
+    return fields;
+  },
+
+  "Field argument of type is required, but it was not provided.": () => { console.log() }
+}
+
+// module.exports = {
+//   /Cannot query field|on type/g: () => { // parse through "Cannot query field \"woobae\" on type \"Character\"." to get woobae}
+//   }
+//   "Cannot query field on type": () => {blah blah blah};
+// }
+
--- package/errorParser.ts
+++ package/errorParser.ts
@@ -1,2 +1,32 @@
 // errorParser.ts
 // identify type of error, error manipulation (classification) {input will be error message, output will be array of unvalidated fields}
+
+// error for fields that don't exist in schema ("code": "GRAPHQL_VALIDATION_FAILED")
+const string1 = "Cannot query field \"woobae\" on type \"Character\"."
+
+// error 
+const string2 = "Field \"post\" argument \"description\" of type \"String!\" is required, but it was not provided."
+
+// error "INTERNAL_SERVER_ERROR"
+const string3 = "WHERE parameter \"characterId\" has invalid \"undefined\" value"
+
+function errorParser(error) {
+
+  const strArr = error.split(' ');
+
+  const notFields = [];
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i][0] !== '"') {
+      notFields.push(strArr[i]);
+    }
+  }
+
+  return notFields.join(' ');
+}
+
+console.log(errorParser(string1));
+console.log(errorParser(string2));
+console.log(errorParser(string3));
+
+//call function at errLibrary[errorParser(string2)]()
