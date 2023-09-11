23a5aeafefbdbefff33b141e0b699d182c233705
Nancy Y <naixin.yang@gmail.com>
2023-09-11 12:47:18 -0500

    finish main part of queryFormatter

--- package/queryFormatter.ts
+++ package/queryFormatter.ts
@@ -19,108 +19,57 @@ const testQuery = `
         description
         test
       }
+      text {
+        content
+      }
     }
   }
 `;
 
-const testFields = ['test'];
+const testFields = ['test', 'text'];
 
+//helper function to remove the field{} in the query
+function remove(field: string, query: string): string {
+  let newQuery = '';
+  if (query.includes(field)) {
+    //regex express to match the word with or without curly braces immediately following it
+    const regex1 = new RegExp(`${field}\\s*\\{`);
+    console.log(regex1.test(query));
+    //1st case, there's no curly braces pair after the field
+    if (!regex1.test(query)) {
+      //   console.log('there is no curly braces right after field');
+      const regex2 = new RegExp(`${field}\\s*`);
+      newQuery = query.replace(regex2, '');
+    } else {
+      //2nd case, there a pair of {} right after the field
+      const regex = new RegExp(`(\\s|\\n)*${field}(\\s|\\n)*\\{[^{}]*\\}`, 'g');
+      newQuery = query.replace(regex, '');
+    }
+  }
+
+  //3rd case, there's no field found
+  else {
+    return query;
+  }
+  //return the modified query
+  console.log(newQuery);
+  return newQuery;
+}
 //after comparison, we need data from the schema, ---fields
 function queryFormatter(query: string) {
+  //find the key word in the testquery matching the string
   let cache = query;
-  //parse the cache to object using graphql parse method, parsed is a AST object, abostract sytax tree
 
   return function (fields: string[]) {
-    let parsed = parse(cache);
-
-    function traverseAST(node: QueryAST, value: string): void {
-      if (node.definitions === value) {
-        //delete the kind
-      }
-      if (node.children) {
-        for (const child of node.children) {
-          traverseAST(child, value);
-        }
-      }
+    let modifiedQuery = cache;
+    for (let i = 0; i < fields.length; i++) {
+      modifiedQuery = remove(fields[i], modifiedQuery);
     }
+    //need to check if the returned modifiedQuery is the same as the origional query
+    return modifiedQuery;
   };
 }
 
 const originalQuery = queryFormatter(testQuery);
-const modifiedQuery = originalQuery(testFields);
-console.log(modifiedQuery);
-
-// query {
-// feed {
-//links {
-//id
-//descripton
-//}
-//}
-//} GRAPH_VALIDATION_ERROR: message
-
-//input
-// fields = ['jeremy', 'nancy']
-
-//output
-// query {
-// feed {
-//links {
-//id
-//}
-//}
-//}
-
-// {
-//   kind: 'Document',
-//   definitions: [
-//     {
-//       kind: 'OperationDefinition',
-//       operation: 'query',
-//       selectionSet: {
-//         kind: 'SelectionSet',
-//         selections: [
-//           {
-//             kind: 'Field',
-//             name: {
-//               kind: 'Name',
-//               value: 'user',
-//             },
-//             arguments: [
-//               {
-//                 kind: 'Argument',
-//                 name: {
-//                   kind: 'Name',
-//                   value: 'id',
-//                 },
-//                 value: {
-//                   kind: 'StringValue',
-//                   value: '123',
-//                 },
-//               },
-//             ],
-//             selectionSet: {
-//               kind: 'SelectionSet',
-//               selections: [
-//                 {
-//                   kind: 'Field',
-//                   name: {
-//                     kind: 'Name',
-//                     value: 'id',
-//                   },
-//                 },
-//                 {
-//                   kind: 'Field',
-//                   name: {
-//                     kind: 'Name',
-//                     value: 'username',
-//                   },
-//                 },
-//               ],
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   ],
-// }
+const newQuery = originalQuery(testFields);
+console.log(newQuery);
