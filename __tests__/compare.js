5f75d77e36f658899ee7a7814aba13ae9ecc35ce
Samuel Ryder <samuelryder@Samuels-MacBook-Pro.local>
2023-09-22 16:28:46 -0400

    completed addition of test instances to compare.js

--- __tests__/compare.js
+++ __tests__/compare.js
@@ -40,7 +40,7 @@ describe('compare tests', () => {
     }
   };
 
-  it('The sample query is valid and the error object is an empty object', () => {
+  xit('The sample query is valid and the error object is an empty object', () => {
 
     const sampleQuery = {
       query: {
@@ -50,10 +50,10 @@ describe('compare tests', () => {
 
     const errorObj = {};
 
-    expect(compare(testSchema, sampleQuery).toEqual(errorObj));
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
   });
 
-  it('Return error object with type properties whose value is an array with invalid field strings on query type', () => {
+  xit('Return error object with type properties whose value is an array with invalid field strings on query type', () => {
 
     const sampleQuery = {
       query: {
@@ -66,16 +66,90 @@ describe('compare tests', () => {
     expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
   });
 
-  it('Return error object with type properties whose value is an array with invalid field strings on mutation type', () => {
+  xit('Return error object when invalid fields occur in two different levels of query depth', () => {
 
     const sampleQuery = {
-      mutation: {
-        createCharacter: {
-
-        }
+      query: {
+        characters: ['id', 'height', 'gender', 'jeremy', { films: ['title', 'director', 'sam'] }]
       }
+    };
+
+    const errorObj = {
+      characters: ['jeremy'],
+      films: ['sam']
     }
 
-  })
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
+  });
+
+  xit('Return error object when invalid fields occur in three different levels of query depth', () => {
+
+    const sampleQuery = {
+      query: {
+        characters: ['id', 'height', 'gender', 'woobae', { films: ['title', 'director', 'sam', { characters: ['jeremy', 'height', 'name'] }] }]
+      }
+    };
+
+    const errorObj = {
+      characters: ['woobae', 'jeremy'],
+      films: ['sam']
+    }
+
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
+  });
+
+  xit('Return error object when invalid fields occur only in first level of query depth, despite query continuing to three levels of depth', () => {
+
+    const sampleQuery = {
+      query: {
+        characters: ['id', 'height', 'gender', 'woobae', { films: ['title', 'director', { characters: ['height', 'name'] }] }]
+      }
+    };
+
+    const errorObj = { characters: ['woobae'] };
+
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
+  });
+
+  xit('Return error object when invalid fields occur only in second level of query depth, despite query continuing to three levels of depth', () => {
+
+    const sampleQuery = {
+      query: {
+        characters: ['id', 'height', 'gender', { films: ['title', 'director', 'woobae', { characters: ['height', 'name'] }] }]
+      }
+    };
+
+    const errorObj = { films: ['woobae'] };
+
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
+  });
+
+  xit('Return error object when invalid fields occur only in third level of query depth', () => {
+
+    const sampleQuery = {
+      query: {
+        characters: ['id', 'height', 'gender', { films: ['title', 'director', { characters: ['height', 'jeremy'] }] }]
+      }
+    };
+
+    const errorObj = { characters: ['jeremy'] };
+
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
+
+  });
+
+  it('Return error object when invalid fields occur only in third level of query depth', () => {
+
+    const sampleQuery = {
+      query: {
+        characters: ['id', 'height', 'gender', { films: ['title', 'director', 'height', { characters: ['height'] }] }]
+      }
+    };
+
+    const errorObj = { films: ['height'] };
+
+    expect(compare(testSchema, sampleQuery)).toEqual(errorObj);
+
+  });
 
 });
